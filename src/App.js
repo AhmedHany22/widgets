4c702bf36c0a06c4bcc7ecc75f8e1d191379f2bc
AmdHany22 <ahmed_hany19@outlook.com>
2021-12-02 01:06:37 +0200

    Creating Search widget

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Accordion from "./components/accordion";
+// import Accordion from "./components/accordion";
+import Search from "./components/search";
 
 const App = () => {
-  const items = [
-    {
-      title: "What is a dog?",
-      content:
-        "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
-    },
-    {
-      title: "What kinds of dogs are there?",
-      content:
-        "There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.",
-    },
-    {
-      title: "How do you acquire a dog?",
-      content:
-        "Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.",
-    },
-  ];
-  return <Accordion items={items} />;
+  // const items = [
+  //   {
+  //     title: "What is a dog?",
+  //     content:
+  //       "A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.",
+  //   },
+  //   {
+  //     title: "What kinds of dogs are there?",
+  //     content:
+  //       "There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.",
+  //   },
+  //   {
+  //     title: "How do you acquire a dog?",
+  //     content:
+  //       "Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.",
+  //   },
+  // ];
+  return <Search />;
 };
 
 export default App;
---
+++ src/components/search.js
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const Search = () => {
+  const [term, setTerm] = useState("Programming");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      const searchId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+      return () => {
+        clearTimeout(searchId);
+      };
+    }
+  }, [term]);
+
+  const renderList = results.map((item) => {
+    return (
+      <div class="item" style={{ borderColor: "DodgerBlue" }}>
+        <div class="right floated content" style={{ margin: "20px 0 10px" }}>
+          <a
+            href={`https://en.wikipedia.org?curid=${item.pageid}`}
+            class="ui button"
+          >
+            Go
+          </a>
+        </div>
+        <div className="content" style={{ margin: "20px 0 10px" }}>
+          <h4 className="ui blue header">{item.title}</h4>
+          <span dangerouslySetInnerHTML={{ __html: item.snippet }} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <React.Fragment>
+      <div className="ui center aligned basic inverted segment">
+        <div className="ui search">
+          <div className="ui icon input">
+            <input
+              type="text"
+              value={term}
+              className="prompt"
+              onChange={(e) => setTerm(e.target.value)}
+              placeholder="Search countries..."
+            />
+            <i className="search icon"></i>
+          </div>
+        </div>
+      </div>
+      <div className="ui middle aligned divided list container">
+        {renderList}
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Search;
